4de1b87b3adcf078b1e25f7b440046ed975664a5
Matěj Holouš <matejholous@seznam.cz>
2024-01-23 11:32:33 +0100

    4 comicst and punč

--- bozenka/miniap/models.py
+++ bozenka/miniap/models.py
@@ -10,4 +10,4 @@ class post(models.Model):
     author = models.CharField(max_length=200,default="x")
     title = models.CharField(max_length=200,default="x")
     mood = models.CharField(max_length=200,default="x")
-    price = models.DecimalField(max_digits=20,decimal_places=0)
+    price = models.DecimalField(max_digits=20,decimal_places=0) 
