d43118d38bef7cc1e632af0011e871813d9402d0
Matěj Holouš <matejholous@seznam.cz>
2024-02-13 11:26:05 +0100

    4 comicst and punč miluju toma

--- bozenka/miniap/views.py
+++ bozenka/miniap/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import random
 
 # Create your views here.
 
 
+
 def models_list(request):
-    return HttpResponse("Testing of views")
+        random_n = random.randint(1, 20)
+        random_n += random.choice([5, 10, 0])
+
+        if random_n == 1:
+            random_n = 2
+            message = "Testing of views 2 Bohužel štěstí nepřálo"
+        else:
+            message = f"Testing of views {random_n}"
+
+        return HttpResponse(message)
